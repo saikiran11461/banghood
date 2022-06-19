3a1db3ebc136439d0a8ccd4fbe228881eeeb738b
saikiran11461 <70562454+saikiran11461@users.noreply.github.com>
2022-06-19 14:43:31 +0530

    done

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter} from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { persistor, store } from './Redux/store';
+import { persistor, store } from './Redux/Store';
 import { PersistGate } from 'redux-persist/integration/react';
 // import ContextProvider from './Context/ContextProvider';
 
